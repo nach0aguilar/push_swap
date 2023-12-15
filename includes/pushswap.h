f303ee8257fa488051e7c7f20360ab5d124a852b
nach0aguilar <nachoaguilarfdz@gmail.com>
2023-12-15 23:41:48 +0100

    Check numbers function created

--- includes/pushswap.h
+++ includes/pushswap.h
@@ -4,6 +4,14 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include "libft/libft.h"
+# include "ft_printf/ft_printf.h"
+
+typedef struct      s_stack
+{
+    int             nb;
+    struct s_stack  *next;
+}                   t_stack;
+
 
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,12 +1,44 @@
-#include "../includes/pushswap.h"
+//#include "../includes/pushswap.h"
+#include <stdio.h>
+#include <unistd.h>
 
-int main(int argc, char **argv)
+static int	checknums(char **nums)
 {
-    if(argc >= 2)
-    {
-        write(1, "Error\n", 6);
-        return (0);
-    }
+	int	i;
+	int	j;
 
+	i = 0;
+	j = 0;
+	while (nums[i])
+	{
+		j = 0;
+		while (nums[i][j])
+		{
+			if (!(nums[i][j] >= '0' || nums[i][j] <= '9') && nums[i][j] != '+'
+				&& nums[i][j] != '-' && nums[i][j] != ' ')
+				return (0);
+			if ((nums[i][j] != '+' || nums[i][j] != '-')
+				&& !(nums[i][j + 1] >= '0' || nums[i][j + 1] <= '9'))
+				return (0);
+			else
+				j++;
+		}
+        i++;
+	}
+	return (1);
+}
 
+// static t_stack	*newstack(char **nums)
+// {
+// }
+
+int	main(int argc, char **argv)
+{
+	if (argc < 2)
+	{
+		write(1, "Error\n", 6);
+		return (0);
+	}
+
+    printf("%d\n", checknums(argv));
 }
