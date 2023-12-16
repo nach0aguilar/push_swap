9eac4044f422fa4acf5d99296baae542a22a7b4e
nach0aguilar <nachoaguilarfdz@gmail.com>
2023-12-16 22:56:09 +0100

    New stack function in process

--- src/main.c
+++ src/main.c
@@ -1,6 +1,36 @@
 //#include "../includes/pushswap.h"
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+
+typedef struct s_stack
+{
+	int				nb;
+	struct s_stack	*next;
+}					t_stack;
+
+int	ft_atoi(const char *str)
+{
+	int				i;
+	int				sign;
+	unsigned int	num;
+
+	i = 0;
+	sign = 1;
+	num = 0;
+	while ((str[i] == 32) || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if ((str[i] == '-') || (str[i] == '+'))
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+		num = num * 10 + (str[i++] - '0');
+	return ((int)(sign * num));
+}
 
 static int	checknums(char **nums)
 {
@@ -14,8 +44,8 @@ static int	checknums(char **nums)
 		j = 0;
 		while (nums[i][j])
 		{
-			if (!(nums[i][j] >= '0' && nums[i][j] <= '9')
-				&& nums[i][j] != '+' && nums[i][j] != '-' && nums[i][j] != ' ')
+			if (!(nums[i][j] >= '0' && nums[i][j] <= '9') && nums[i][j] != '+'
+				&& nums[i][j] != '-' && nums[i][j] != ' ')
 				return (0);
 			else if ((nums[i][j] == '+' || nums[i][j] == '-') && !(nums[i][j
 					+ 1] >= '0' && nums[i][j + 1] <= '9'))
@@ -28,17 +58,39 @@ static int	checknums(char **nums)
 	return (1);
 }
 
-// static t_stack	*newstack(char **nums)
+// static t_stack	*newstack(char *num)
 // {
+// 	t_stack	*stack;
+
+// 	stack = (t_stack *)malloc(sizeof(t_stack));
+// 	if (!stack)
+// 		return (NULL);
+// 	stack->nb = ft_atoi(num);
+// 	return (stack);
 // }
 
 int	main(int argc, char **argv)
 {
-	if (argc < 2)
+	t_stack	*stack_A;
+	//t_stack	*stack_B;
+
+	if (argc < 2 || checknums(argv) == 0)
 	{
 		write(1, "Error\n", 6);
 		return (0);
 	}
+	int i = 1;
+	while(argv[i])
+	{
+		stack_A->nb = atoi(argv[i]);
+		stack_A = stack_A->next;
+		i++;
+	}
+	stack_A = NULL;
 
-	printf("%d\n", checknums(argv));
+	while(stack_A != NULL)
+	{
+		printf("%d\n", stack_A->nb);
+		stack_A = stack_A->next;
+	}
 }
