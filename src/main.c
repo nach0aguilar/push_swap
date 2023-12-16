6318018fc14d369292d1f0f839d677dc1168cd79
nach0aguilar <nachoaguilarfdz@gmail.com>
2023-12-16 10:16:55 +0100

    Check numbers function fixed

--- src/main.c
+++ src/main.c
@@ -7,23 +7,23 @@ static int	checknums(char **nums)
 	int	i;
 	int	j;
 
-	i = 0;
+	i = 1;
 	j = 0;
 	while (nums[i])
 	{
 		j = 0;
 		while (nums[i][j])
 		{
-			if (!(nums[i][j] >= '0' || nums[i][j] <= '9') && nums[i][j] != '+'
-				&& nums[i][j] != '-' && nums[i][j] != ' ')
+			if (!(nums[i][j] >= '0' && nums[i][j] <= '9')
+				&& nums[i][j] != '+' && nums[i][j] != '-' && nums[i][j] != ' ')
 				return (0);
-			if ((nums[i][j] != '+' || nums[i][j] != '-')
-				&& !(nums[i][j + 1] >= '0' || nums[i][j + 1] <= '9'))
+			else if ((nums[i][j] == '+' || nums[i][j] == '-') && !(nums[i][j
+					+ 1] >= '0' && nums[i][j + 1] <= '9'))
 				return (0);
 			else
 				j++;
 		}
-        i++;
+		i++;
 	}
 	return (1);
 }
@@ -40,5 +40,5 @@ int	main(int argc, char **argv)
 		return (0);
 	}
 
-    printf("%d\n", checknums(argv));
+	printf("%d\n", checknums(argv));
 }
